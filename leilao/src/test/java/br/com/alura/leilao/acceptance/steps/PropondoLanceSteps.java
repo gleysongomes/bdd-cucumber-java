da95bd5d91af070fdc7ee97b79d60a3345a3e7ba
Gleyson Gomes <fgleysondasilva@gmail.com>
2024-08-30 23:28:35 -0300

    propondo lances ao leilão

--- leilao/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ leilao/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -14,24 +14,52 @@ import io.cucumber.java.pt.Quando;
 public class PropondoLanceSteps {
 
 	private Lance lance;
-	
+
 	private Leilao leilao;
-	
+
+	private Lance lance10;
+
+	private Lance lance15;
+
 	@Dado("um lance valido")
 	public void dado_um_lance_valido() {
 		Usuario usuario = new Usuario("teste");
 		lance = new Lance(usuario, BigDecimal.TEN);
+		leilao = new Leilao("Tablet teste");
 	}
 
-	@Quando("propoe o lance")
-	public void quando_propoe_o_lance() {
-		leilao = new Leilao("Tablet teste");
+	@Quando("propoe ao leilao")
+	public void quando_propoe_ao_leilao() {
 		leilao.propoe(lance);
 	}
-	
+
 	@Entao("o lance e aceito")
 	public void entao_o_lance_e_aceito() {
 		assertEquals(1, leilao.getLances().size());
 		assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+	}
+
+	@Dado("varios lances validos")
+	public void varios_lances_validos() {
+		Usuario usuario1 = new Usuario("Teste1");
+		lance10 = new Lance(usuario1, BigDecimal.TEN);
+
+		Usuario usuario2 = new Usuario("Teste2");
+		lance15 = new Lance(usuario2, new BigDecimal("15.0"));
+
+		leilao = new Leilao("Tablet teste");
+	}
+
+	@Quando("propoe varios lances ao leilao")
+	public void propoe_varios_lances_ao_leilao() {
+		leilao.propoe(lance10);
+		leilao.propoe(lance15);
+	}
+
+	@Entao("os lances sao aceitos")
+	public void os_lances_sao_aceitos() {
+		assertEquals(2, leilao.getLances().size());
+		assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+		assertEquals(new BigDecimal("15.0"), leilao.getLances().get(1).getValor());
 	}
 }
