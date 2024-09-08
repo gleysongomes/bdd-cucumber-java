3231ad08a38359d48ded604cd9a2a4a449034676
Gleyson Gomes <fgleysondasilva@gmail.com>
2024-09-08 07:51:06 -0300

    exemplos e tabela de dados

--- leilao/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ leilao/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -3,10 +3,15 @@ package br.com.alura.leilao.acceptance.steps;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
@@ -17,15 +22,18 @@ public class PropondoLanceSteps {
 
 	private Leilao leilao;
 
-	private Lance lance10;
+	private List<Lance> lances;
 
-	private Lance lance15;
+	@Before
+	public void setUp() {
+		this.lances = new ArrayList<>();
+		leilao = new Leilao("Tablet teste");
+	}
 
 	@Dado("um lance valido")
 	public void dado_um_lance_valido() {
 		Usuario usuario = new Usuario("teste");
 		lance = new Lance(usuario, BigDecimal.TEN);
-		leilao = new Leilao("Tablet teste");
 	}
 
 	@Quando("propoe ao leilao")
@@ -39,27 +47,57 @@ public class PropondoLanceSteps {
 		assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
 	}
 
-	@Dado("varios lances validos")
-	public void varios_lances_validos() {
-		Usuario usuario1 = new Usuario("Teste1");
-		lance10 = new Lance(usuario1, BigDecimal.TEN);
-
-		Usuario usuario2 = new Usuario("Teste2");
-		lance15 = new Lance(usuario2, new BigDecimal("15.0"));
-
-		leilao = new Leilao("Tablet teste");
+	@Dado("um lance de {double} reais do usuario {string}")
+	public void um_lance_de_reais_do_usuario(Double valor, String nomeUsuario) {
+		Lance lance = new Lance(new Usuario(nomeUsuario), new BigDecimal(valor));
+		lances.add(lance);
 	}
 
 	@Quando("propoe varios lances ao leilao")
 	public void propoe_varios_lances_ao_leilao() {
-		leilao.propoe(lance10);
-		leilao.propoe(lance15);
+		this.lances.forEach(lance -> leilao.propoe(lance));
 	}
 
 	@Entao("os lances sao aceitos")
 	public void os_lances_sao_aceitos() {
-		assertEquals(2, leilao.getLances().size());
-		assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
-		assertEquals(new BigDecimal("15.0"), leilao.getLances().get(1).getValor());
+		assertEquals(this.lances.size(), leilao.getLances().size());
+		assertEquals(this.lances.get(0).getValor(), leilao.getLances().get(0).getValor());
+		assertEquals(this.lances.get(1).getValor(), leilao.getLances().get(1).getValor());
+	}
+
+	@Dado("um lance invalido de {double} reais do usuario {string}")
+	public void um_lance_invalido_de_reais_do_usuario(Double valor, String nomeUsuario) {
+		lance = new Lance(new BigDecimal(valor));
+	}
+
+	@Entao("o lance nao e aceito")
+	public void o_lance_nao_e_aceito() {
+		assertEquals(0, leilao.getLances().size());
+	}
+
+	@Entao("o segundo lance nao e aceito")
+	public void o_segundo_lance_nao_e_aceito() {
+		assertEquals(1, leilao.getLances().size());
+		assertEquals(this.lances.get(0).getValor(), leilao.getLances().get(0).getValor());
+	}
+
+	@Dado("dois lances")
+	public void dois_lances(DataTable dataTable) {
+		List<Map<String, String>> valores = dataTable.asMaps();
+		for (Map<String, String> mapa : valores) {
+			String valor = mapa.get("valor");
+			String nome = mapa.get("nomeUsuario");
+			Lance lance = new Lance(new Usuario(nome), new BigDecimal(valor));
+			lances.add(lance);
+		}
+	}
+
+	@Dado("varios lances validos")
+	public void varios_lances_validos(List<String> valores) {
+		Usuario usuario = new Usuario("teste");
+		for (String valor : valores) {
+			Lance lance = new Lance(usuario, new BigDecimal(valor));
+			lances.add(lance);
+		}
 	}
 }
--- leilao/src/test/resources/features/propondo_lance.feature
+++ leilao/src/test/resources/features/propondo_lance.feature
@@ -7,7 +7,39 @@ Cenario: Propondo um unico lance valido
 	Quando propoe ao leilao
 	Entao o lance e aceito
 
-	Cenario: Propondo varios lances validos
-	Dado varios lances validos
+Cenario: Propondo varios lances validos
+	Dado um lance de 10.0 reais do usuario "teste1"
+	E um lance de 15.0 reais do usuario "teste2"
 	Quando propoe varios lances ao leilao
 	Entao os lances sao aceitos
+
+Esquema do Cenario: Propondo um lance invalido
+	Dado um lance invalido de <valor> reais do usuario '<nomeUsuario>'
+	Quando propoe ao leilao
+	Entao o lance nao e aceito
+
+Exemplos:
+	| valor   | nomeUsuario  |
+	|   0.0   |   teste1     |
+	|   -1.0  |   teste2     |
+
+Esquema do Cenario: Propondo uma sequencia de lances
+	Dado um lance de 10.0 reais do usuario "teste1"
+	E um lance de 15.0 reais do usuario "teste1"
+	Quando propoe varios lances ao leilao
+	Entao o segundo lance nao e aceito
+
+Esquema do Cenario: Propondo uma sequencia de lances
+	Dado dois lances
+	| valor   | nomeUsuario  |
+	|   10.0  |   teste1     |
+	|   15.0  |   teste1     |
+	Quando propoe varios lances ao leilao
+	Entao o segundo lance nao e aceito
+
+Esquema do Cenario: Propondo uma sequencia de lances
+	Dado varios lances validos
+	|   10.0  |
+	|   15.0  |
+	Quando propoe varios lances ao leilao
+	Entao o segundo lance nao e aceito
